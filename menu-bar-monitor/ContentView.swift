8cd7b641885922b13718d369b784783abb552ed3
Mark Yang <mark.s.yang@gmail.com>
2026-06-09 22:34:06 +0800

    Initial Commit

---
+++ menu-bar-monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  menu-bar-monitor
+//
+//  Created by Mark S.Yang on 2026/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ menu-bar-monitor/menu_bar_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_bar_monitorApp.swift
+//  menu-bar-monitor
+//
+//  Created by Mark S.Yang on 2026/6/9.
+//
+
+import SwiftUI
+
+@main
+struct menu_bar_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
